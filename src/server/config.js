876fbab47c7160304ceed7cd3139f0df346f6c9e
Daniel Rocha <to.danielrp@gmail.com>
2019-01-01 13:39:47 -0400

    middlewares config

--- src/server/config.js
+++ src/server/config.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const exhbs = require('express-handlebars');
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+
 
 module.exports = app => {
     app.set('port', process.env.PORT || '3000');
+
     app.set('views', path.join(__dirname, 'views'));
     app.engine('.hbs', exhbs({
         defaultLayout: 'main',
@@ -13,6 +18,11 @@ module.exports = app => {
     }));
 
     app.set('view engine', '.hbs');
-    
+
+    app.use(morgan('dev'));
+    app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
     return app;
 }
