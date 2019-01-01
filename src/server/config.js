9854c95746e261959eef81d859220836f1c4e433
Daniel Rocha <to.danielrp@gmail.com>
2019-01-01 12:54:02 -0400

    view engine handlebars setup

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const exhbs = require('express-handlebars');
+
 module.exports = app => {
     app.set('port', process.env.PORT || '3000');
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exhbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }));
+
+    app.set('view engine', '.hbs');
+    
     return app;
 }
