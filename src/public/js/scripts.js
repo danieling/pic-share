a93fb55296cdbe5b451664a4cb8cd6471a1c8b81
Daniel Rocha <to.danielrp@gmail.com>
2019-01-22 13:07:59 -0400

    delete images implies that comments, file image  are gone too

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -7,3 +7,28 @@ $('#btn-like').click(function(e) {
             $('.likes-count').text(data.likes)
         })
 })
+
+$('#btn-delete').click(function(e) {
+    e.preventDefault()
+    let $this = $(this)
+   const response = confirm("Are you sure you want to delete this image?")
+   if(response) {
+       let imgId = $this.data('id')
+       $.ajax({
+        url: '/images/' + imgId,
+        type: 'DELETE'
+       })
+       .done(function(result) {
+           $this.removeClass('btn-danger').addClass('btn-success')
+           $this.find('i').removeClass('fa-times').addClass('fa-check')
+           $this.append('<span>Deleted!</span>')
+       })
+   }
+})
+
+$('#post-comment').hide()
+
+$('#btn-toggle-comment').click(e => {
+    e.preventDefault()
+    $('#post-comment').slideToggle()
+})
